6b5b1087bfdeb744d6a07b8839ffc55cf59bc0ed
Woxez <139673932+Serpentsqw@users.noreply.github.com>
2024-12-03 11:56:44 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hacked By Akhi</title>
+    <style>
+      body {
+        font-family: 'Courier New';
+        background-color: black;
+        color: white
+      }
+      
+      h1 {
+        font-size: 60px;
+        color: red;
+      }
+      
+      b {
+        color: white;
+      }
+    </style>
+  </head>
+  <body>
+    <center>
+      <img style="border-radius: 50%;" src="https://cdn.glitch.global/d2087574-c915-4519-8997-d517546017b1/IHY8E05o_400x400.jpg?v=1727040034648">
+      <h1>Hacked By Akhi</h1>
+      <br>
+      <h2 style="font-size: 50px;">Akhi Muslim Defacer</h2>
+      <br>
+      <b>Onlar, lânete uğramış olarak cehennemde ebediyen kalacaklardır. Azapları asla hafifletilmeyecek, geciktirilmeyecek ve kendilerine özür dilemeleri için mühlet de verilmeyecektir.</b>
+      <p>Bakara 162. Ayet</p>
+    </center>
+  </body>
+</html>
